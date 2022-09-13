3bfeacf4c652700deac29638391e791e52f28902
Gabi Vawdrey <ruth.sousa15@gmail.com>
2022-09-12 19:11:12 -0600

    Last changes and prepare for heroku

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const path = require('path');
 const cors = require("cors");
 const { SERVER_PORT } = process.env;
 const { seed, getNinas, createNina, getNina } = require("./controller.js");
-
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '..', 'client/build')));
 
 // DEV
 app.post("/seed", seed);
@@ -15,5 +16,9 @@ app.post("/seed", seed);
 app.post("/ninas", createNina);
 app.get("/ninas", getNinas);
 app.get("/ninas/:id", getNina);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '/client/build/index.html'));
+});
 
 app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`));
